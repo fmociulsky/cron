4e79ca888980ed96d3dfddcd2a6254af13a4902f
Fernando Mociulsky <fmociulsky@garbarino.com.ar>
2020-08-16 19:46:28 -0300

    envio de mails

--- app.js
+++ app.js
@@ -4,33 +4,62 @@ const request = require("request");
 const parser = require("node-html-parser");
 const {GoogleSpreadsheet} = require('google-spreadsheet');
 const format = require('date-format');
+const nodemailer = require('nodemailer')
 const creds = require('./client_secret.json');
 const {promisify} = require('util');
+require('dotenv').config();
 app = express();
 
-//cron.schedule('*/5 * * * * ', parsearHTML);
+var transporter = nodemailer.createTransport({
+    service: process.env.MAIL_SERVICE,
+    auth: {
+      user: process.env.MAIL_FROM,
+      pass: process.env.MAIL_PASS
+    }
+  });
+
+cron.schedule('*/1 * * * * ', parsearHTML);
 
 function parsearHTML() {
-    request({uri: "http://www.turismocity.com.ar"}, 
+    const url = process.env.URL;
+    console.log("Buscando en la url: " + url);
+    request({uri: url}, 
         function(error, response, body) {
             const fecha = format("dd-MM-yyyy hh:mm:ss",new Date());
             const root = parser.parse(body);
             const figures = root.querySelectorAll("figure");
             const rows = [];
-            for (let index = 0; index < figures.length; index++) {
-                const spans = figures[index].querySelectorAll("span");
-                if(spans.length > 0){
-                    const row = {
-                        Fecha: fecha,
-                        Desde: spans[0].text.replace('\n','').replace('\n','').trim(),
-                        Hasta: spans[1].text.replace('\n','').replace('\n','').trim(),
-                        Precio: spans[2].text.replace('\n','').replace('\n','').replace('$','').replace('.','').trim() 
+            if(figures.length == 0){
+                for (let index = 0; index < figures.length; index++) {
+                    const spans = figures[index].querySelectorAll("span");
+                    if(spans.length > 0){
+                        const row = {
+                            Fecha: fecha,
+                            Desde: spans[0].text.replace('\n','').replace('\n','').trim(),
+                            Hasta: spans[1].text.replace('\n','').replace('\n','').trim(),
+                            Precio: spans[2].text.replace('\n','').replace('\n','').replace('$','').replace('.','').trim() 
+                        }
+                        rows.push(row);
+                    }   
+                }
+                accessSpreadsheets(rows);
+                console.log(fecha+ " - Se encontraron " + rows.length + " ofertas");
+            }else{
+                var mailOptions = {
+                    from: process.env.MAIL_FROM,
+                    to: process.env.MAIL_TO,
+                    subject: 'Error al obtener las ofertas',
+                    text: 'Por algun error no se pudieron obtener las ofertas. Hablar con Fer'
+                  };
+                transporter.sendMail(mailOptions, function(error, info){
+                    if (error) {
+                      console.log(error);
+                    } else {
+                      console.log('Email enviado a : ' + process.env.MAIL_TO);
                     }
-                    rows.push(row);
-                }   
+                });
             }
-            accessSpreadsheets(rows);
-            console.log(fecha+ " - " + rows.length);
+            
     });
 }
 
@@ -42,7 +71,6 @@ async function accessSpreadsheets(rows){
       });
     await doc.loadInfo();
     const sheet = doc.sheetsByIndex[0]; // or use doc.sheetsById[id]
-
     await sheet.addRows(rows);
 }
 
